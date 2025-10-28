28819804b131f2bf3eb4f012392e06fc888c50df
Sua Bae <bsa0530@gmail.com>
2025-10-28 23:16:51 +0900

    fix(be, auth): 사용자 로그인 기능 구현

--- backend/src/auth/api/auth.controller.ts
+++ backend/src/auth/api/auth.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
-import {AuthService} from "../application/auth.service";
-import {SignUpUserCommand} from "../application/dto/sign-up-user.command";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from '../application/auth.service';
+import { SignUpUserCommand } from '../application/dto/sign-up-user.command';
+import { SignInUserCommand } from '../application/dto/sign-in-user.command';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService :AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   @UsePipes(ValidationPipe)
   async signUp(@Body() command: SignUpUserCommand) {
     return this.authService.signUp(command);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(ValidationPipe)
+  async signIn(@Body() command: SignInUserCommand) {
+    return this.authService.signIn(command);
   }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,21 @@ import { User } from './domain/user.entity';
 import { AuthController } from './api/auth.controller';
 import { AuthService } from './application/auth.service';
 import { UserRepositoryPersistence } from './infrastructure/user.repository.persistence';
-import {IUserRepository} from "./domain/user.repository";
+import { IUserRepository } from './domain/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'testrppgfaicalexpressionanalysis',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [
     AuthService,
     { provide: IUserRepository, useClass: UserRepositoryPersistence },
