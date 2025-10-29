41d6eb89fa6d9175f067f57d67f557b7f62742e9
배 수아 <suby00@ejbt.co.kr>
2025-10-29 09:26:35 +0900

    style(code): 코드 스타일 정리

--- backend/src/auth/infrastructure/user.repository.persistence.ts
+++ backend/src/auth/infrastructure/user.repository.persistence.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import {User} from "../domain/user.entity";
-import {IUserRepository} from "../domain/user.repository";
+import { User } from '../domain/user.entity';
+import { IUserRepository } from '../domain/user.repository';
 
 @Injectable()
 export class UserRepositoryPersistence implements IUserRepository {
