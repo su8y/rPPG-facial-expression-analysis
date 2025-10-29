67d27a5c703cc9ccd8b5505383fc960220884518
Sua Bae <bsa0530@gmail.com>
2025-10-29 15:11:53 +0900

    [#4] feat(fe, common): 비동기 로딩 컴포넌트 구현 (#17)
    * chore(code): 안쓰는 코드 정리
    
    * chore(config): 프로젝트 라이브러리 설치 및 설정
    
    * feat(fe, common): 비동기 로딩 컴포넌트 구현
    
    ---------
    
    Co-authored-by: 배 수아 <suby00@ejbt.co.kr>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode, Suspense} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from "./App.tsx";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'; // Import QueryClient and QueryClientProvider
+import '@mantine/core/styles.css';
+
+import {MantineProvider} from '@mantine/core';
+import LoadingIndicator from "./components/LoadingIndicator.tsx";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false, // 기본적으로는 retry 제외
+        },
+    }
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider>
+                <Suspense fallback={<LoadingIndicator/>}>
+                    <App/>
+                </Suspense>
+            </MantineProvider>
+        </QueryClientProvider>
+    </StrictMode>,
+);
