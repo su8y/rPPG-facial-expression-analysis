a8fa31538c19995db34c8f17e866d07a6b88579a
배 수아 <suby00@ejbt.co.kr>
2025-10-29 12:17:34 +0900

    feat(be, dashboard): 대시보드 API 구현

---
+++ backend/src/dashboard/api/dashboard.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { DashboardService } from '../application/dashboard.service';
+import { AuthGuard } from '../../auth/application/auth.guard';
+import { DashboardData } from '../domain/rppg.type';
+
+@Controller('api/sessions')
+@UseGuards(AuthGuard)
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+  @Get('result-report')
+  async getResultReport(): Promise<DashboardData> {
+    return this.dashboardService.getResultReport();
+  }
+}
