872f28a995d039c8e08d997aaaa089ca0243af3b
배 수아 <suby00@ejbt.co.kr>
2025-10-29 14:33:55 +0900

    chore(config): 프로젝트 라이브러리 설치 및 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from "./App.tsx";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'; // Import QueryClient and QueryClientProvider
+import '@mantine/core/styles.css';
+
+import {MantineProvider} from '@mantine/core';
+
+const queryClient = new QueryClient(); // Create a new QueryClient instance
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}> {/* Wrap App with QueryClientProvider */}
+            <MantineProvider>
+                <App/>
+            </MantineProvider>
+        </QueryClientProvider>
+    </StrictMode>,
+);
